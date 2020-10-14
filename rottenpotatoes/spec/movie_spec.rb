15c7522a1190d664be215e4fd9bdfcf4703b870c
EC2 Default User <ec2-user@ip-172-31-73-0.ec2.internal>
2020-10-14 00:59:57 +0000

    rspec tests working for Movie model

--- rottenpotatoes/spec/movie_spec.rb
+++ rottenpotatoes/spec/movie_spec.rb
@@ -1,31 +1,29 @@
 require 'rails_helper'
 describe Movie do
-    
-    it 'should find a movie with exact same director' do
-        kk = Movie.create(title: 'KingKong', director: 'Peter Jackson')
-        lotr = Movie.create(title: 'Lord of the Rings', director: 'Peter Jackson')
-        expect(Movie.similar_movies(kk.id)).to eq([kk, lotr])
-    end
-    
-    it 'should find all movies with the exact same director' do
-        kk = Movie.create(title: 'KingKong', director: 'Peter Jackson')
-        lotr = Movie.create(title: 'Lord of the Rings', director: 'Peter Jackson')
-        me = Movie.create(title: 'Mortal Engines', director: 'Peter Jackson')
-        expect(Movie.similar_movies(kk.id)).to eq([kk, lotr, me])
-    end
-    
-    
-    it 'should find movies with case sensitive director' do
-        kk = Movie.create(title: 'KingKong', director: 'PeTeR JacKson')
-        lotr = Movie.create(title: 'Lord of the Rings', director: 'Peter Jackson')
-        me = Movie.create(title: 'Mortal Engines', director: 'Peter Jackson')
-        expect(Movie.similar_movies(lotr.id)).to eq([lotr, me])
-    end
-    
-    
+    describe '.similar_movies' do
+        # movie1 = Movie.create(title: 'King Kong', director: 'Peter Jackson')
+        # movie2 = Movie.create(title: 'Lord of the Rings', director: 'Peter Jackson') 
+        # movie3 = Movie.create(title: 'Kangaroo Jack', director: 'David McNally')
+        # movie4 = Movie.create(title: 'Overworked CS Student')
+        #Added these movies in, but I found that every time I ran rspec, it was creating a new movie
+        #so this was my ducttape fix
+        movie1 = Movie.where(title: 'King Kong')[0]
+        movie2 = Movie.where(title: 'Lord of the Rings')[0] 
+        movie3 = Movie.where(title: 'Kangaroo Jack')[0]
+        movie4 = Movie.where(title: 'Overworked CS Student')[0]
 
-    it 'should return nil if unable to find another movie with the same director' do
-        ocs = Movie.create(title: 'Overworked CS Student')
-        expect(Movie.similar_movies(ocs.id)).to eq(nil)
+        context 'move has a director' do
+            it 'finds similar movies based on director' do
+                expect(Movie.similar_movies(movie1.id)).to eq([movie1, movie2])
+            end
+            it 'doesnt find movies by different directors' do
+                expect(Movie.similar_movies(movie1.id)).to_not include([movie3, movie4])
+            end
+        end
+        context 'movie does not have a director' do
+            it 'defaults to nill (sad path)' do
+                expect(Movie.similar_movies(movie4.id)).to eq(nil)
+            end
+        end
     end
 end 
